98827c028e9e51f206432aa0eed586992edfef98
classlion <classlion1@gmail.com>
2020-06-08 18:39:16 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,13 @@
+const toggle = document.getElementsByClassName("navbar-toggle");
+
+toggle[0].addEventListener("click", function(event) {
+  event.preventDefault();
+  this.classList.toggle("is-active");
+});
+
+const closeNavbarMobile = document.getElementsByClassName("close");
+
+closeNavbarMobile[0].addEventListener("click", function(event) {
+  event.preventDefault();
+  toggle[0].classList.remove("is-active");
+});
